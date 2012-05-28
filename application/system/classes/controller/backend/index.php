8e8f8012d6a907cb9893916ffc5ab9388390d348
Florian Koerner <contact@florian-koerner.eu>
2012-05-29 00:15:43 +0200

    * Test Frontent-Controller * Test Backend-Controller

---
+++ application/system/classes/controller/backend/index.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Backend_Index extends Controller_Backend_Template {
+
+	public function action_index()
+	{
+		// do nothing
+	}
+
+}
