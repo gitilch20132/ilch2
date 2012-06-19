4d37f5760d343c6724150a771b72586c896825d9
KoernerWS <contact@florian-koerner.eu>
2012-06-19 09:38:22 +0200

    Neue Version vom System und Pluto-Theme

--- application/system/views/backend/index/index.php
+++ application/system/views/backend/index/index.php
@@ -1,140 +1,69 @@
-<div class="box">
-	<header>
-		Willkommen
-	</header>
-	<section>
-		<p>
-			Du bist hier im Backend vom neuen Ilch Pluto. Dieses
-			CMS ist der schnelle und flexible Ablöser von dem
-			Ilch Clan. Wenn du deine Clan-Module vermisst, siehe
-			doch mal unter „Modulverwaltung“ nach. Dort findest
-			du eine vernünftige Anzahl von Ilch Modulen, die du
-			kinderleicht nachinstallieren kannst. Auch kannst du
-			dort Updates von Modulen und vom Ilch System selbst
-			installieren lassen.
-		</p>
-		<p>
-			Siehe dich einfach ein bisschen um, du wirst dich
-			sicherlich schnell zurechtfinden. Dein Ilch-Team ;)
-		</p>
-	</section>
-</div>
-    			
-<div class="box">
-	<header>
-		Bootstrap
-	</header>
-	<section>    			
-		<ul class="breadcrumb">
-		  <li>
-		    <a href="#">Home</a> <span class="divider">/</span>
-		  </li>
-		  <li>
-		    <a href="#">Library</a> <span class="divider">/</span>
-		  </li>
-		  <li class="active">Data</li>
-		</ul>
-		
-		<h2>A sample Ilch Application</h2>
-		<p>
-			This is an example bootstrap view
-		</p>
-		
-		<blockquote>
-		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis.</p>
-		  <small>Someone famous</small>
-		</blockquote>
-		
-		<p>
-			<span class="label label-success">Success</span> For example, <code>section</code> should be wrapped as inline.
-		</p>
-		
-		<pre>
-		  &lt;p&gt;Sample text here...&lt;/p&gt;
-		</pre>
-		
-		<table class="table table-striped">
-		        <thead>
-		          <tr>
-		            <th>#</th>
-		            <th>First Name</th>
-		            <th>Last Name</th>
-		            <th>Username</th>
-		          </tr>
-		        </thead>
-		        <tbody>
-		          <tr>
-		            <td>1</td>
-		            <td>Mark</td>
-		            <td>Otto</td>
-		            <td>@mdo</td>
-		          </tr>
-		          <tr>
-		            <td>2</td>
-		            <td>Jacob</td>
-		            <td>Thornton</td>
-		            <td>@fat</td>
-		          </tr>
-		          <tr>
-		            <td>3</td>
-		            <td>Larry</td>
-		            <td>the Bird</td>
-		            <td>@twitter</td>
-		          </tr>
-		        </tbody>
-		      </table>
-		      
-		<div class="alert alert-success">
-		  <i class="icon-search"></i> Yeah, Yeah, Yeah! ;-)
+<div class="clearfix">
+	<div class="left-col large">
+		<div class="box">
+			<div class="head">
+				Willkommen
+			</div>
+			<div class="content">
+				<p>
+					Du bist hier im Backend vom neuen Ilch Pluto. Dieses
+					CMS ist der schnelle und flexible Ablöser von dem
+					Ilch Clan. Wenn du deine Clan-Module vermisst, siehe
+					doch mal unter „Modulverwaltung“ nach. Dort findest
+					du eine vernünftige Anzahl von Ilch Modulen, die du
+					kinderleicht nachinstallieren kannst. Auch kannst du
+					dort Updates von Modulen und vom Ilch System selbst
+					installieren lassen.
+				</p>
+				<p>
+					Siehe dich einfach ein bisschen um, du wirst dich
+					sicherlich schnell zurechtfinden. Dein Ilch-Team ;)
+				</p>
+			</div>
 		</div>
-		      
-		<form class="well">
-		  <label>Label name</label>
-		  <input type="text" class="span3" placeholder="Type something…">
-		  <span class="help-block">Example block-level help text here.</span>
-		  <label class="checkbox">
-		    <input type="checkbox"> Check me out
-		  </label>
-		  <button type="submit" class="btn">Submit</button>
-		</form>
-		
-		<form class="well form-inline">
-		  <input type="text" class="input-small" placeholder="Email">
-		  <input type="password" class="input-small" placeholder="Password">
-		  <label class="checkbox">
-		    <input type="checkbox"> Remember me
-		  </label>
-		  <button type="submit" class="btn">Sign in</button>
-		</form>
-		
-		<a class="btn" href="">Link</a>
-		<button class="btn" type="submit">
-		  Button
-		</button>
-		<input class="btn" type="button"
-		         value="Input">
-		<input class="btn" type="submit"
-		         value="Submit">
-		         <br /><br />
-		<ul class="nav nav-tabs">
-		  <li class="active">
-		    <a href="#">Home</a>
-		  </li>
-		  <li><a href="#">Link 2</a></li>
-		  <li><a href="#">Link 3</a></li>
-		</ul>
-		
-		<div class="pagination">
-		  <ul>
-		    <li><a href="#">Prev</a></li>
-		    <li class="active">
-		      <a href="#">1</a>
-		    </li>
-		    <li><a href="#">2</a></li>
-		    <li><a href="#">3</a></li>
-		    <li><a href="#">4</a></li>
-		    <li><a href="#">Next</a></li>
-		  </ul>
+		<div class="box">
+			<div class="head">
+				Willkommen
+			</div>
+			<div class="content">
+				<p>
+					Du bist hier im Backend vom neuen Ilch Pluto. Dieses
+					CMS ist der schnelle und flexible Ablöser von dem
+					Ilch Clan. Wenn du deine Clan-Module vermisst, siehe
+					doch mal unter „Modulverwaltung“ nach. Dort findest
+					du eine vernünftige Anzahl von Ilch Modulen, die du
+					kinderleicht nachinstallieren kannst. Auch kannst du
+					dort Updates von Modulen und vom Ilch System selbst
+					installieren lassen.
+				</p>
+				<p>
+					Siehe dich einfach ein bisschen um, du wirst dich
+					sicherlich schnell zurechtfinden. Dein Ilch-Team ;)
+				</p>
+			</div>
 		</div>
-	</section>
+	</div>
+	<div class="right-col small">
+		<div class="box">
+			<div class="head">
+				Willkommen
+			</div>
+			<div class="content">
+				<p>
+					Du bist hier im Backend vom neuen Ilch Pluto. Dieses
+					CMS ist der schnelle und flexible Ablöser von dem
+					Ilch Clan. Wenn du deine Clan-Module vermisst, siehe
+					doch mal unter „Modulverwaltung“ nach. Dort findest
+					du eine vernünftige Anzahl von Ilch Modulen, die du
+					kinderleicht nachinstallieren kannst. Auch kannst du
+					dort Updates von Modulen und vom Ilch System selbst
+					installieren lassen.
+				</p>
+				<p>
+					Siehe dich einfach ein bisschen um, du wirst dich
+					sicherlich schnell zurechtfinden. Dein Ilch-Team ;)
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
