0f57a59c72dc61f15c215488ba92eae161820edd
KoernerWS <contact@florian-koerner.eu>
2012-08-25 13:18:15 +0200

    Media-Modul Update

--- application/system/classes/controller/frontend/index.php
+++ application/system/classes/controller/frontend/index.php
@@ -1,9 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Frontend_Index extends Controller_Frontend_Template {
+class Controller_Frontend_Index extends Template_Frontend {
 
 	public function action_index()
 	{
+        Jelly::query('user_auth_service')->with('user')->select();
+        
 		$this->template->content = View::factory('frontend/index/index');
 	}
 
